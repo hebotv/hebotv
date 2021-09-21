7707cfe8fb9da0621040d87b5de3a9b42c827a49
Embbnux <embbnux@embbnux.com>
2021-09-21 18:15:02 +0800

    use global storage hook

--- packages/web/src/components/App/index.js
+++ packages/web/src/components/App/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import useGlobalStorage from 'use-global-storage';
 import { makeStyles } from '@material-ui/core/styles';
 import Backdrop from '@material-ui/core/Backdrop';
 import CircularProgress from '@material-ui/core/CircularProgress';
@@ -30,13 +31,17 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const useStorage = useGlobalStorage({
+  storageOptions: { name: 'hebo-tv' },
+});
+
 function App() {
   const classes = useStyles();
   const history = useHistory();
-  const [source, setSource] = useState('https://iptv-org.github.io/iptv/countries/cn.m3u');
-  const [channels, setChannels] = useState([]);
-  const [channel, setChannel] = useState({ uri: '' });
-  const [loading, setLoading] = React.useState(false);
+  const [source, setSource] = useStorage('source', 'https://iptv-org.github.io/iptv/index.m3u');
+  const [channels, setChannels] = useStorage('channels', []);
+  const [channel, setChannel] = useStorage('channel', { uri: '' });
+  const [loading, setLoading] = useState(false);
   const loadSource = async () => {
     setLoading(true);
     try {
